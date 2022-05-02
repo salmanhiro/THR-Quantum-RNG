ede284f06597a23ba27513b5ea8551e978fb206e
Salman Chen <salman.consulting@outlook.com>
2022-05-02 17:22:30 +0700

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.8-slim-buster
+
+RUN apt-get update -y
+RUN apt install libgl1-mesa-glx wget libglib2.0-0 -y
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY . .
+
+WORKDIR .
+
+EXPOSE 8501
+
+CMD ["streamlit", "run", "app.py"]
---
+++ app.py
@@ -0,0 +1,39 @@
+from logging.config import valid_ident
+import qrng
+import streamlit as st
+
+if 'thr' not in st.session_state:
+    st.session_state.thr = 0
+
+def on_click_generate_number():
+    result = qrng.get_random_double(0,1)
+    thr_amount = filter_result(result)
+    st.session_state.thr = thr_amount
+
+def filter_result(value):
+    if value < 0.5:
+        return 1000
+    elif value < 0.7:
+        return 2000
+    elif value < 0.9:
+        return 5000
+    elif value < 0.99:
+        return 10000
+    else:
+        return 50000
+
+def object_main():
+    qrng.set_provider_as_IBMQ('')
+    qrng.set_backend()
+
+    st.title("THR Gacha Generator")
+    st.write("Aplikasi untuk menentukan jumlah THR secara random dengan quantum random number")
+    st.write("Disclaimer: THR ditanggung pengguna aplikasi ini; salman tidak bertanggungjawab memberi THR")
+
+    st.button('Klik disini', on_click=on_click_generate_number)
+    st.write('Selamat anda mendapat ', st.session_state.thr)
+    st.write("Selamat lebaran mohon maaf lahir batin")
+
+
+if __name__ == '__main__':
+    object_main()
